2d5196803515db910d4646d44582b9c42cd2c873
andrzejpudlik <andrzejpudlik98@gmail.com>
2022-02-06 20:34:39 +0100

    Server connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -9,5 +10,10 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+    .then(() => console.log('DB Connection'))
+    .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
